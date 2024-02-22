525bd527e1be437f3774b4c298ab771c262d9949
Lina Yoy <rogovaya.angel@yandex.ru>
2024-02-22 23:02:36 +0500

    index.js

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const mysql = require("mysql2");
-const express = require( 'express')
+const mysql = require("mysql2")
+const express = require('express')
 const app = express()
 const port = 3000
+
 
 const pool = mysql.createPool({
     connectionLimit: 5,
     host: "localhost",
     user: "root",
-    database: "car_dealership",
+    database: "carDealership",
     password: ""
 });
 
-app.get("/car_dealership", function(req, res){
-    pool.query("SELECT * FROM car_dealership", (err, data) => {
-        if(err) return console.log(err);
+
+app.get('/employees', function (req, res) {
+    pool.query("SELECT * FROM  employees" + req.params.id, (err, data) => {
+        if(err) return  console.log(err);
         res.json( {
-            car_dealership: data
+            employees: data
         });
     });
-});
+  });
 
-
-
-
-app.listen(port, () => {
+  app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+  })
